ee7d7dc57845774ed931e1e9a8c8ab28fbcc6939
DP <deepakpattnayak2013@gmail.com>
2023-03-09 20:35:40 +0530

    resized the dataset

---
+++ imageResize.py
@@ -0,0 +1,15 @@
+# import the modules
+import os
+from os import listdir
+from PIL import Image
+from tqdm import tqdm
+
+def imageResize(location,newLocation):
+	for images in tqdm(os.listdir(location),desc='processing...'):
+		if images.endswith(".png"):
+			img = Image.open(r""+location+'/'+images)
+			img = img.resize((350,350))
+			img.save(f'C:\\Users\\ASUS\\Desktop\\SDP\\modifiedSignatures\\{newLocation}\\'+'resized'+images.capitalize()+'.png')
+
+imageResize('C:\\Users\\ASUS\\Desktop\\SDP\\signatures\\full_forg','modifiedForged')
+imageResize('C:\\Users\\ASUS\\Desktop\\SDP\\signatures\\full_org','modifiedOG')
